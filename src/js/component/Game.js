db0a6de03c6fa98c9c29bf3455ae40085032fd9a
cgabrielP <cgabriel2303@gmail.com>
2022-09-22 13:40:59 +0000

    input

--- src/js/component/Game.js
+++ src/js/component/Game.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import {calculateWinner} from "../helper";
 import Board from "./Board"
+import Input from "./input"
 
 const Game = ()=>{
     const[history, setHistory] = useState([Array(9).fill('')])
     const[stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXisNext] = useState(true);
     const winner = calculateWinner(history[stepNumber]);
+    const [name1, setName1]= useState('')
+    const [name2, setName2]= useState('')
+
     const xO = xIsNext ? "X" : "O";
+    const playerName = xIsNext ? (name1) : (name2);
+    
 
     const handleClick = (i) => {
         const historyPoint = history.slice(0, stepNumber + 1);
@@ -27,7 +33,7 @@ const Game = ()=>{
 
 const renderMoves = () =>
     history.map((_step, move) =>{
-        const destination = move ? `Go to move #${move}` :"Go to Start"
+        const destination = move ? `Go to move #${move}` :"REINICIAR"
         return(
             <li key={move}>
                 <button onClick={()=> jumpTo(move)}>{destination}</button>
@@ -37,14 +43,22 @@ const renderMoves = () =>
 
     return (
         <>
-          <h1>React Tic Tac Toe - With Hooks</h1>
+          <h1 className="text-center">React Tic Tac Toe </h1>
+          <div className='row'>
+          <div className='col-6'>
+          <Input type={"text"} id={'player2'} name={'name'} value={name1} placeholder={'Jugador 1'} onChange={(event) => setName1(event.target.value)} className={"form-control my-2"} />
+          </div>
+          <div className='col-6'>
+          <Input type={"text"} id={'player1'} name={'name'} value={name2} placeholder={'Jugador 2'} onChange={(event) => setName2(event.target.value)} className={"form-control my-2"} />
+          </div>
+          </div>
           <Board squares={history[stepNumber]} onClick={handleClick} />
           <div className="info-wrapper">
             <div>
-              <h3>History</h3>
+              
               {renderMoves()}
             </div>
-            <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+            <h3>{winner ? "Ganador " + winner : "Turno de " + (playerName)}</h3>
           </div>
         </>
       );
---
+++ src/js/component/input.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Input = ({type, id, name, value, placeholder, onChange, className})=>{
+    return(
+        <input type={type} id={id} name={name} value={value} placeholder={placeholder} onChange={onChange} className={className}/>
+    )
+}
+export default Input
